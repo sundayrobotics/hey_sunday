4548461f0dd94b6e0cd57ce6bf5251ec74780bf9
sundayrobotics <info@sundayrobotics.com>
2022-12-05 12:12:59 +0300

    Uncomment main code

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-# import the neccesary packages
-import struct #unpacks audio input
-import pyaudio #create an audio stream from mic connected to raspberrypi
-import pvporcupine # Engine allows us to listen to hotwords
+# # import the neccesary packages
+# import struct #unpacks audio input
+# import pyaudio #create an audio stream from mic connected to raspberrypi
+# import pvporcupine # Engine allows us to listen to hotwords
 
-#create vars to store handles for porcupine
-porcupine = None
-pa = None
-audio_stream = None
+# #create vars to store handles for porcupine
+# porcupine = None
+# pa = None
+# audio_stream = None
 
-access_key="LJAhNEy++D83aj0clU0fN67MJnAnyWhEKW+dC4efSBaHD5VkaZKKXA=="
+# access_key="LJAhNEy++D83aj0clU0fN67MJnAnyWhEKW+dC4efSBaHD5VkaZKKXA=="
 
-try:
-    porcupine = pvporcupine.create(
-        access_key=access_key,
-        keywords=["computer","porcupine"]    
-    )
+# try:
+#     porcupine = pvporcupine.create(
+#         access_key=access_key,
+#         keywords=["computer","porcupine"]    
+#     )
 
-    pa = pyaudio.PyAudio()
+#     pa = pyaudio.PyAudio()
 
-    audio_stream = pa.open(
-        rate=porcupine.sample_rate,
-        channels=1,
-        format=pyaudio.paInt16,
-        input=True,
-        frames_per_buffer=porcupine.frame_length
-    )
+#     audio_stream = pa.open(
+#         rate=porcupine.sample_rate,
+#         channels=1,
+#         format=pyaudio.paInt16,
+#         input=True,
+#         frames_per_buffer=porcupine.frame_length
+#     )
 
-    while True:
-        pcm=audio_stream.read(porcupine.frame_length)
-        pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+#     while True:
+#         pcm=audio_stream.read(porcupine.frame_length)
+#         pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
 
-        keyword_index = porcupine.process(pcm)
+#         keyword_index = porcupine.process(pcm)
 
-        if keyword_index >= 0:
-            print("active...")
+#         if keyword_index >= 0:
+#             print("active...")
 
-finally:
-    if porcupine is not None:
-        porcupine.delete()
+# finally:
+#     if porcupine is not None:
+#         porcupine.delete()
 
-    if audio_stream is not None:
-        audio_stream.delete()
+#     if audio_stream is not None:
+#         audio_stream.delete()
 
-    if pa is not None:
-        pa.terminate()
+#     if pa is not None:
+#         pa.terminate()
+
+print("Docker Hey_sunday running")
